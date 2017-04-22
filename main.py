cc61c0629a1a0b5e34f70a6e01698746543389f8
Heyjoy <iheyjoy@gmail.com>
2017-04-23 00:42:03 +0800

    add some new

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.image as mpimg
 from utils import *
 from datafield import *
 import classifier as clf
+import extraction
 def pipeline(image):
 
     # 3. slide_window to find the cars
@@ -14,3 +15,10 @@ def pipeline(image):
 # 1. decide what features to use
 # 2. train the classifier
 clf.datasetInit()
+rand_car = np.random.choice(len(cars))
+rand_notcar = np.random.choice(len(notcars))
+this_car = mpimg.imread(cars[rand_car])
+this_notcar = mpimg.imread(notcars[rand_notcar])
+
+features = extraction.extract_features([cars[rand_car]],color_space='YCrCb',hog_channel='ALL',plot =True)
+#twoImagePlot(this_car,this_notcar,title1="car",title2 ="no car",path="output_images/car_not_car.png")
--- utils.py
+++ utils.py
@@ -13,18 +13,7 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return draw_img
-def color_hist(img, nbins=32, bins_range=(0, 256)):
-    # Compute the histogram of the RGB channels separately
-    rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
-    ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
-    bhist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
-    # Generating bin centers
-    bin_edges = rhist[1]
-    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
-    # Concatenate the histograms into a single feature vector
-    hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
-    # Return the individual histograms, bin_centers and feature vector
-    return rhist, ghist, bhist, bin_centers, hist_features
+
 def find_matches(img, template_list):
     # Define an empty list to take bbox coords
     bbox_list = []
@@ -80,24 +69,7 @@ def plot3d(pixels, colors_rgb,
         c=colors_rgb.reshape((-1, 3)), edgecolors='none')
 
     return ax  # return Axes3D object for further manipulation
-def bin_spatial(img, color_space='RGB', size=(32, 32)):
-    # Convert image to new color space (if specified)
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else: feature_image = np.copy(img)
-    # Use cv2.resize().ravel() to create the feature vector
-    features = cv2.resize(feature_image, size).ravel()
-    # Return the feature vector
-    return features
+
 
 def data_look(car_list, notcar_list):
     data_dict = {}
@@ -116,33 +88,7 @@ def data_look(car_list, notcar_list):
 
 
 
-def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
-                        hist_bins=32, hist_range=(0, 256)):
-    # Create a list to append feature vectors to
-    features = []
-    # Iterate through the list of images
-    for file in imgs:
-        # Read in each one by one
-        image = mpimg.imread(file)
-        # apply color conversion if other than 'RGB'
-        if cspace != 'RGB':
-            if cspace == 'HSV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-            elif cspace == 'LUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-            elif cspace == 'HLS':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-            elif cspace == 'YUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-        else: feature_image = np.copy(image)
-        # Apply bin_spatial() to get spatial color features
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        # Apply color_hist() also with a color space option now
-        hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
-        # Append the new feature vector to the features list
-        features.append(np.concatenate((spatial_features, hist_features)))
-    # Return list of feature vectors
-    return features
+
 def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
                     xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
     # If x and/or y start/stop positions not defined, set to image size
@@ -182,52 +128,6 @@ def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
             window_list.append(((startx, starty), (endx, endy)))
     # Return the list of windows
     return window_list
-
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9,
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
-    #1) Define an empty list to receive features
-    img_features = []
-    #2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else: feature_image = np.copy(img)
-    #3) Compute spatial features if flag is set
-    if spatial_feat == True:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        #4) Append features to list
-        img_features.append(spatial_features)
-    #5) Compute histogram features if flag is set
-    if hist_feat == True:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        #6) Append features to list
-        img_features.append(hist_features)
-    #7) Compute HOG features if flag is set
-    if hog_feat == True:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:,:,channel],
-                                    orient, pix_per_cell, cell_per_block,
-                                    vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient,
-                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        #8) Append features to list
-        img_features.append(hog_features)
-
-    #9) Return concatenated array of features
-    return np.concatenate(img_features)
 
 # Define a function you will pass an image
 # and the list of windows to be searched (output of slide_windows())
@@ -355,19 +255,8 @@ def draw_labeled_bboxes(img, labels):
     # Return the image
     return img
 
-def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
-    if vis == True:
-        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False,
-                                  visualise=True, feature_vector=False)
-        return features, hog_image
-    else:
-        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False,
-                       visualise=False, feature_vector=feature_vec)
-        return features
 # plot method
-def imagePlot(image1,image2,title1 = 'Image1', title2 = 'Image2', color1=None,color2 ='gray',path = 'output_images/default.png'):
+def twoImagePlot(image1,image2,title1 = 'Image1', title2 = 'Image2', color1=None,color2 ='gray',path = 'output_images/default.png'):
     plt.figure(1)
     plt.axis('off')
     plt.subplot(121)
